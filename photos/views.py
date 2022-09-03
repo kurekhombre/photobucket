75c8a35bcfe50a824bc64839e21f50ef7516c138
Karol <kurekdev@gmail.com>
2022-09-03 22:02:32 +0200

    add form feature and sort by category commit

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 
 
 # Create your views here.
 def gallery(request):
+    category = request.GET.get('category')
+    if category is None:
+        photos = Photo.objects.all()
+
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
+
     return render(request, 'photos/gallery.html', {
         'categories': categories,
         'photos': photos,
@@ -20,4 +27,27 @@ def view_photo(request, pk):
 
 
 def add_photo(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image
+        )
+
+        return redirect('gallery')
+
+    return render(request, 'photos/add.html',{
+        'categories': categories
+    })
